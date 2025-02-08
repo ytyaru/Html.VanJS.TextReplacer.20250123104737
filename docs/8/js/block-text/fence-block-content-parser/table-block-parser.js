681ffd9b4d5624e7dde6a3f68270a8e4e624cf34
ytyaru <yttry0@gmail.com>
2025-02-08 15:09:05 +0900

    8 textbase table

--- docs/8/js/block-text/fence-block-content-parser/table-block-parser.js
+++ docs/8/js/block-text/fence-block-content-parser/table-block-parser.js
@@ -3,12 +3,22 @@ class TableBlockParser { // フェンスブロック"""author=著者名等をHTM
     constructor() {
 //        this._parsers = []
     }
-    is(block, bi) { return 'quote'===block.fence.type }
+    //is(block, bi) { return 'quote'===block.fence.type }
+    is(block, bi) {return Array.isArray(block.fence.ary) && 0 < block.fence.ary.length && block.fence.ary[0].startsWith('tbl') }
     //parse(block, bi) { return this.#blockquote(block, bi) }
     //#blockquote(block, bi) {
     parse(block, bi) {
         console.log(block, bi)
-        return [{}]
+        const tsv = this.#getTsv(block, bi)
+        block.parse.data = this.#getRows(tsv)
+//        const types = this.#getTypes(tsv)
+//        const rows = tsv.slice([Math.max(0, typeIdx, defIdx)+1])
+        //rows.map((r,i)=>types[i%types.length].deserialize(r))
+//        return rows.map(r=>r.map((c,i)=>types[i%types.length].deserialize(c)))
+
+        console.log(tsv)
+        console.log(block.parse.data)
+        return block.parse.data
         /*
         const param = {aurhot:{name:null, url:null}, work:{name:null, url:null, season:null}, hero:{name:null, url:null}, writer:{comment:null}}
         if (block.fence.arg.ary) {
@@ -31,6 +41,37 @@ class TableBlockParser { // フェンスブロック"""author=著者名等をHTM
 //        return `<blockquote data-bi="${bi}"><code>${block.body.text}</code></pre>`
         return code
         */
+    }
+    #getTsv(block, bi) {return block.body.text.split('\n').map(line=>line.split('\t'))}
+    //#getTypes(tsv) {
+    #getRows(tsv) {
+        if (tsv.length < 1) {throw new TypeError(`少なくとも二行以上あるはずですが、それより少ないです。textbaseのtable複数行表記は一行目に列名を記入すべきです。二行目、三行目は任意で型名、初期値をセットできます。それぞれ行頭に:,=を記入してください。それ以降はデータを入力します。行と列のデリミタは改行コードとタブコードです。:${tsv.length}`)}
+//        const typeTxts = (tsv[1][0].startsWith(':')) ? 
+        let [typeIdx, defIdx] = [-1, -1];
+        if (tsv[1][0].startsWith(':')) {tsv[1][0] = tsv[1][0].slice(1); typeIdx=1;}
+        //if ([1,2].some(r=>tsv[r][0].startsWith('=')){tsv[r][0]=tsv[r][0].slice(1); defIdx=r;}
+             if (tsv[1][0].startsWith('=')){tsv[1][0]=tsv[1][0].slice(1); defIdx=1;}
+        else if (tsv[2][0].startsWith('=')){tsv[2][0]=tsv[2][0].slice(1); defIdx=2;}
+
+        const typeTxts = 0 < typeIdx ? tsv[typeIdx] : Array.of(...tsv[0]).map(v=>null);
+        const defTxts = 0 < defIdx ? tsv[defIdx] : Array.of(...tsv[0]).map(v=>null);
+        console.log(typeTxts , defTxts)
+        if (typeTxts.length !== defTxts.length) {throw new TypeError(`型と初期値の列数が一致しません。:${typeTxts.length},${defTxts.length}}`)}
+        const rows = tsv.slice([Math.max(0, typeIdx, defIdx)+1])
+        //const types = typeTxts.map((t,i)=>textbase.TypeParser.getType(t, defTxts[i]))
+        const types = typeTxts.map((t,i)=>{
+            const type = textbase.TypeParser.getType(t, defTxts[i])
+            return type ? type : this.#getLiteralType(rows, i, defTxts)
+        })
+        console.log(types)
+        //rows.map((r,i)=>types[i%types.length].deserialize(r))
+        return rows.map(r=>r.map((c,i)=>types[i%types.length].deserialize(c)))
+    }
+    #getLiteralType(rows, colIdx, defTxts) {
+        for (let row of rows) {
+            if (''!==row[colIdx]) {return textbase.LiteralType.get(row[colIdx])}
+        }
+        return textbase.StringDataType(defTxts[i])
     }
     #getFigCaption(param) {
         const author = this.#getNameUrlHtml(param, 'author')
